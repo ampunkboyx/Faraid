4e6940c45ea8c0e4ac4b67a494d73d82b996e375
ampunkboyx <afif.mynet@gmail.com>
2024-07-30 06:27:32 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ from django.conf import settings
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, True)
+    DEBUG=(bool, False)
 )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
